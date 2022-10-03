869c7e77ab0ba85cbb2c6aec08c4fb4623a8b279
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2022-10-03 19:39:02 +0530

    Input-output

---
+++ CINCOUT.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+//In this challenge, we practice reading input from stdin and printing output to stdout.
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;  
+    cin >>a>>b>>c;
+    cout<<(a+b+c);
+    return 0;
+}
